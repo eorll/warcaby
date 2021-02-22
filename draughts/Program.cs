ee583be768d9704fc005bd3b3d0f290ce92b2440
eorll <robert0dadon@gmail.com>
2021-02-22 19:57:45 +0100

    start

---
+++ draughts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace draughts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
